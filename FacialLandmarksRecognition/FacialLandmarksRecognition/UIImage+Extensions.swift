7a993d56a38be9061efb2cf51ac59ef85681a3f4
Jason <sanchezjason@me.com>
2024-03-05 16:54:25 -0700

    swiftui/ML_facial_landmarks- FacialLandmarksView

---
+++ FacialLandmarksRecognition/FacialLandmarksRecognition/UIImage+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIImage+Extensions.swift
+//  FacialLandmarksRecognition
+//
+//  Created by Cause  I'm Electric on 3/5/24.
+//
+
+import Foundation
+import UIKit
+import Vision
+
+extension UIImage {
+    
+    func drawLandmarksOnImage(observations: [VNFaceObservation]) -> UIImage? {
+        
+        return nil
+    }
+}
