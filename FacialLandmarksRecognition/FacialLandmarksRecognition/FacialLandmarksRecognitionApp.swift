1f0093e155deff0fb1a8f65044d145fa597fa767
Jason <sanchezjason@me.com>
2024-03-05 16:50:19 -0700

    swiftui/ML_facial_landmarks- FacialLandmarksView, Image assets added

---
+++ FacialLandmarksRecognition/FacialLandmarksRecognition/FacialLandmarksRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialLandmarksRecognitionApp.swift
+//  FacialLandmarksRecognition
+//
+//  Created by Cause  I'm Electric on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FacialLandmarksRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FacialLandmarksRecognition/FacialLandmarksRecognition/FacialLandmarksView.swift
@@ -0,0 +1,18 @@
+//
+//  FacialLandmarksView.swift
+//  FacialLandmarksRecognition
+//
+//  Created by Cause  I'm Electric on 3/5/24.
+//
+
+import SwiftUI
+
+struct FacialLandmarksView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FacialLandmarksView()
+}
